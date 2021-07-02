cd11b67b727ecafd7812c3969e882fba77d0edf3
Jeff <jefercasjn@gmail.com>
2021-07-02 14:44:49 +1000

    message error

--- module1_task1/build.sh
+++ module1_task1/build.sh
@@ -7,13 +7,11 @@ while [[ $# -gt 0 ]]; do
   case $key in
     -b|build)
       shift # past argument
-      hugo
-      echo -e ""
+      hugo > /dev/null 2>&1
       ;;
     -c|clean)
       shift # past argument
       rm -rf dist/*
-      echo -e ""
       ;;
     -N=*|POST_NAME=*)
       POSTNAME="${key#*=}"
@@ -29,7 +27,6 @@ while [[ $# -gt 0 ]]; do
           hugo new posts/"$POSTNAME.md"  $POSTTITLE
           # Now read the file and change title
           sed -i "s/${POSTNAME}/${POSTTITLE}/gi" "./content/posts/${POSTNAME}.md"
-          echo -e ""
       fi
       ;;
     *)    # unknown option
