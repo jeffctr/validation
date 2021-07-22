518ed00f18c6ed119f2555b8d6bff2630a2ecb11
Jeff <jefercasjn@gmail.com>
2021-07-22 10:59:45 +1000

    updating hugo version because 0.46 want' creating the index.html file

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y wget make
+wget https://github.com/gohugoio/hugo/releases/download/v0.86.0/hugo_0.86.0_Linux-64bit.deb
+dpkg -i hugo_0.86.0_Linux-64bit.deb
+apt-get install -fy
 make build
+rm -rf hugo_0.86.0_Linux-64bit.deb
